f3c0e4165d4fd2700b04a9baf01e27190a95af9d
nyashaChiza <nchizampeni@gmail.com>
2021-06-13 10:26:49 +0200

    g

--- API/script.js
+++
@@ -1,8 +0,0 @@
-// we fetch our box
-const box = document.body.querySelector("div.box")
-
-// on animation complete we tint the box red
-box.addEventListener("transitionend", () => box.style.backgroundColor = "red")
-
-// and start our animation
-requestAnimationFrame(() => box.style.transform = "translate(0px, 100px)")
